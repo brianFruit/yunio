fe1824e3efed9fef9777f6b3d57ae519ba2b2009
Brian Xia <brianfruitose@gmail.com>
2019-09-04 02:19:43 -0700

    Upgrade ServerComm

--- ServerComm.py
+++ ServerComm.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python
-
-"""
-Copyright (C) 2017-2018 Brian Xia
-You may use, distribute and modify this code under the
-terms of the license, which is included in the project
-root directory.
-"""
-
 import urllib2
+import urlparse
+import urllib
 import json
 import threading
 import time
@@ -18,10 +12,10 @@ import sys
 sys.path.insert(0, '/usr/lib/python2.7/bridge/')
 from bridgeclient import BridgeClient as bridgeclient
 
-
 NUMIO = 20
-# Endpoint to receive Arduino I/O values
-URL = "http://[server_ip_address]:port/path/"
+SERVER_IP = None
+SERVER_PORT = None
+
 
 ATmega_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ATmega_socket.connect(('localhost', 6571))
@@ -30,53 +24,100 @@ io_state = {}
 for io in range(NUMIO):
     io_state[str(io)] = None
 
-def pushUpdate():
+def push_update():
+    global SERVER_IP
+    while SERVER_IP is None or SERVER_PORT is None:
+        time.sleep(0.1) 
+
+    db_endpoint = "http://" + SERVER_IP + ":" + str(SERVER_PORT) + "/arduino-to-db"
+
     value = bridgeclient() 
     global io_state
 
     run = True
     while run:
-        post = False
+        # post = False
         for io in io_state:
             new_val = value.get(io)
             old_val = io_state.get(io)
             if new_val != old_val:
                 io_state.update({io: new_val})
-                post = True
+                # post = True
             time.sleep(0.05)
 
-        if post:
-            print io_state
-            try:
-                io_state_json =  json.dumps(io_state)
-                post = urllib2.Request(URL, io_state_json, {'Content-Type': 'application/json'})
-                resp = urllib2.urlopen(post)
-                print resp.read()
-            except urllib2.HTTPError, e:
-                print 'HTTPError = ' + str(e.code)
-            except urllib2.URLError, e:
-                print 'URLError = ' + str(e.reason)
-            except Exception:
-                import traceback
-                print 'generic exception: ' + traceback.format_exc()
+        # if post:
+        print "POSTING\n", io_state, "\nTO ENDPOINT ", db_endpoint
+        try:
+            io_state_json =  json.dumps(io_state)
+            post = urllib2.Request(db_endpoint, io_state_json, {'Content-Type': 'application/json'})
+            resp = urllib2.urlopen(post, timeout=5)
+            print resp.read()
+        except urllib2.HTTPError, e:
+            print 'HTTPError = ' + str(e.code)
+        except urllib2.URLError, e:
+            print 'URLError = ' + str(e.reason)
+        except Exception:
+            import traceback
+            print 'generic exception: ' + traceback.format_exc()
                 
         # run = False
 
 
-# Thread to constantly update I/O pin values
-t = threading.Thread(target=pushUpdate)
+t = threading.Thread(target=push_update)
 t.start()
 
 
-# Light weight server to listen for command and then route to ATmega
 class RestHTTPRequestHandler(BaseHTTPRequestHandler):
+    
+    @staticmethod
+    def parse_query(path):
+        parsed_path = urlparse.urlparse(path)
+        queries = parsed_path.query.split("&")
+        query_dict = {}
+        for q in queries:
+            key, value = q.split("=")
+            query_dict[key] = value
+        return query_dict
+
     def do_GET(self):
-        cmd = self.path.split("/")[1] + "\n"
-        ATmega_socket.send(cmd)
-        self.send_response(200)
-        self.end_headers()
+        global SERVER_IP
+        global SERVER_PORT
+        if SERVER_IP is not None or SERVER_PORT is not None:
+            try:
+                cmd = urllib.unquote(self.path.split("/")[1]) + "\n"
+                ATmega_socket.send(cmd)
+                self.send_response(200)
+                self.end_headers()
+            except Exception as e:
+                print e
+        else:
+            try:
+                queries = self.parse_query(self.path)
+                print queries
+                SERVER_IP = queries["ip_address"]
+                SERVER_PORT = queries["port"]
+            except Exception as e:
+                self.send_response(417)
+                self.end_headers()
+                print e
+
         return
 
+    # def do_POST(self):
+    #     new_id = max(filter(lambda x: x['id'], TODOS))['id'] + 1
+    #     form = cgi.FieldStorage(fp=self.rfile,
+    #                        headers=self.headers, environ={
+    #                             'REQUEST_METHOD':'POST', 
+    #                             'CONTENT_TYPE':self.headers['Content-Type']
+    #                        })
+    #     new_title = form['title'].value
+    #     new_todo = {'id': new_id, 'title': new_title}
+    #     TODOS.append(new_todo)
+ 
+    #     self.send_response(201)
+    #     self.end_headers()
+    #     self.wfile.write(json.dumps(new_todo))
+    #     return
  
 httpd = HTTPServer(('0.0.0.0', 9898), RestHTTPRequestHandler)
 while True:
